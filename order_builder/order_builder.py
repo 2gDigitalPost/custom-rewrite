d81e3094f1c5c8544769587e564846e753393680
Tyler Standridge <tstand90@gmail.com>
2016-08-25 11:26:49 -0700

    Removed unused title functions

--- order_builder/order_builder.py
+++ order_builder/order_builder.py
@@ -285,108 +285,6 @@ class OrderBuilderWdg(BaseRefreshWdg):
 
         return packages_list
 
-    def setup_html_list_for_title_orders(self):
-        """
-        Add the titles that go into the order to an HTML style unordered list
-
-        :return: HtmlElement.ul containing the titles
-        """
-
-        title_order_list = HtmlElement.ul()
-        title_order_list.add_style('list-style-type', 'none')
-
-        for title_order in self.titles_in_order:
-            title_order_name_div = DivWdg()
-            title_order_name_div.add_style('font-weight', 'bold')
-            title_order_name_div.add(title_order.get('name'))
-
-            title_order_description_div = DivWdg()
-            title_order_description_div.add_style('font-style', 'italic')
-            title_order_description_div.add(title_order.get('description'))
-
-            title_order_priority_div = DivWdg()
-            title_order_priority_div.add('Priority: {0}'.format(title_order.get('priority')))
-
-            title_order_due_date_div = DivWdg()
-
-            if title_order.get('due_date'):
-                title_order_due_date_div.add('Due: {0}'.format(title_order.get('due_date')))
-
-            external_rejection_button = ButtonNewWdg(title='External Rejection', icon='RED_BOMB')
-            external_rejection_button.add_behavior(self.get_external_rejection_button_behavior())
-            external_rejection_button.add_style('display', 'inline-block')
-
-            element_evaluation_button = ButtonNewWdg(title='Element Evaluation', icon='REPORT')
-            element_evaluation_button.add_behavior(self.get_element_evaluation_button_behavior(title_order.get('title_code')))
-            element_evaluation_button.add_style('display', 'inline-block')
-
-            note_button = ButtonNewWdg(title='Add Note', icon='NOTE')
-            note_button.add_behavior(obu.get_add_notes_behavior(title_order.get_search_key()))
-            note_button.add_style('display', 'inline-block')
-
-            instructions_button = ButtonNewWdg(title='Instructions', icon='CONTENTS')
-            instructions_button.add_behavior(self.get_title_instructions_wdg(title_order.get_search_key()))
-            instructions_button.add_style('display', 'inline-block')
-
-            change_due_date_button = ButtonNewWdg(title='Change Due Date', icon='DATE')
-            change_due_date_button.add_behavior(self.get_change_due_date_behavior(title_order.get_code(),
-                                                                                  title_order.get('due_date')))
-            change_due_date_button.add_style('display', 'inline-block')
-
-            assign_pipeline_button = ButtonNewWdg(title='Assign Pipeline', icon='PIPELINE')
-            assign_pipeline_button.add_behavior(get_assign_pipeline_behavior(title_order.get_search_key()))
-            assign_pipeline_button.add_style('display', 'inline-block')
-
-            add_task_button = ButtonNewWdg(title='Add Task', icon='INSERT')
-            add_task_button.add_behavior(self.get_add_task_behavior(title_order.get_search_key()))
-            add_task_button.add_style('display', 'inline-block')
-
-            button_row_div = SpanWdg()
-            button_row_div.add_style('display', 'inline-block')
-            button_row_div.add(external_rejection_button)
-            button_row_div.add(element_evaluation_button)
-            button_row_div.add(note_button)
-            button_row_div.add(instructions_button)
-            button_row_div.add(change_due_date_button)
-            button_row_div.add(assign_pipeline_button)
-            button_row_div.add(add_task_button)
-
-            title_div = DivWdg()
-            title_div.add_style('background-color', '#d9edcf')
-            title_div.add_style('padding', '10px')
-            title_div.add_style('border-radius', '10px')
-
-            title_div.add(title_order_name_div)
-            title_div.add(title_order_description_div)
-            title_div.add(title_order_priority_div)
-            title_div.add(title_order_due_date_div)
-            title_div.add(button_row_div)
-
-            tasks = obu.get_tasks_for_title_order(title_order)
-
-            title_task_div = DivWdg()
-            title_task_div.add_style('width', '100%')
-            title_task_div.add_style('margin-left', '30px')
-
-            # If there are tasks associated to the title_order, add them as a sub-list below it
-            if tasks:
-                task_list = DivWdg()
-
-                for task in tasks:
-                    task_div = DivWdg()
-                    task_div.add(self.get_task_div(task))
-                    task_list.add(task_div)
-
-                title_task_div.add(task_list)
-
-            title_order_div = DivWdg()
-            title_order_div.add(title_div)
-            title_order_div.add(title_task_div)
-
-            title_order_list.add(title_order_div)
-
-        return title_order_list
-
     @staticmethod
     def get_add_titles_behavior(order_code):
         behavior = {
@@ -453,29 +351,6 @@ catch(err) {
     spt.app_busy.hide();
     spt.alert(spt.exception.handler(err));
         }''' % title_code
-        }
-
-        return behavior
-
-    @staticmethod
-    def get_title_instructions_wdg(code):
-        """
-        Load the instructions for the title.
-
-        :param code: title_order_code (unique ID)
-        :return: Behavior (Instructions Widget)
-        """
-        behavior = {
-            'css_class': 'clickme',
-            'type': 'click_up',
-            'cbjs_action': '''
-try {
-    spt.api.load_popup('Instructions', 'order_builder.TitleOrderInstructionsWdg', {'search_key': '%s'});
-}
-catch(err) {
-    spt.app_busy.hide();
-    spt.alert(spt.exception.handler(err));
-}''' % code
         }
 
         return behavior
