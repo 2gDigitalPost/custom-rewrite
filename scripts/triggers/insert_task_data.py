ff007f366f302172dfd841743c466d5a2ca98451
Tyler Standridge <tstand90@gmail.com>
2016-09-12 08:37:53 -0700

    Updated Task Data trigger to only work on type twog/component

--- scripts/triggers/insert_task_data.py
+++ scripts/triggers/insert_task_data.py
@@ -3,12 +3,16 @@ def main(server=None, input_data=None):
         input_data = {}
 
     task_sobject = input_data.get('sobject')
-    task_code = task_sobject.get('code')
 
-    search_type = 'twog/task_data'
-    task_data = {'task_code': task_code}
+    if task_sobject and task_sobject.get('search_type') == u'twog/component?project=twog':
+        task_code = task_sobject.get('code')
 
-    server.insert(search_type, task_data)
+        task_data_name = 'Data for Task: {0}'.format(task_code)
+
+        search_type = 'twog/task_data'
+        task_data = {'task_code': task_code, 'name': task_data_name}
+
+        server.insert(search_type, task_data)
 
 
 if __name__ == '__main__':
