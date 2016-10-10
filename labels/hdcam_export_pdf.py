c1fead3886574206ceea70726d34e984fe098ebf
Tyler Standridge <tstand90@gmail.com>
2016-10-10 16:12:08 -0700

    Section One function

--- labels/hdcam_export_pdf.py
+++ labels/hdcam_export_pdf.py
@@ -13,30 +13,9 @@ from reportlab.platypus.flowables import HRFlowable
 from reportlab.pdfgen import canvas as reportlab_canvas
 from reportlab.lib.units import cm, inch
 
+
 def main():
-    c = reportlab_canvas.Canvas("rotate.pdf")
-
-    c.saveState()
-    c.setFont("Helvetica-Oblique", 8)
-    c.translate(.25 * inch, 8.75 * inch)
-    c.drawString(0, 2.6 * inch, "Sony")
-
-    c.setFont("Helvetica", 12)
-    c.drawString(0, 2.4 * inch, "That's My Boy")
-
-    c.setFont("Helvetica", 7)
-    c.drawString(0, 2.25 * inch, "Version: Theatrical")
-    c.drawString(0, 2.10 * inch, "Latin American Spanish Foreign Language Master")
-    c.drawString(0, 1.95 * inch, "Aspect Ratio: 16x9 1.78 Full Frame")
-    c.drawString(0, 1.8 * inch, "Textless: Textless at Tail")
-
-    c.setFont("Helvetica", 6)
-
-    style_sheet = getSampleStyleSheet()
-    style = style_sheet['BodyText']
-    paragraph_style = ParagraphStyle('paraStyle')
-    paragraph_style.fontSize = 6
-    paragraph_style.leading = 8
+    canvas = reportlab_canvas.Canvas("hdcam.pdf")
 
     channel_one_text = 'Latin American Spanish Left Total'
     channel_two_text = 'Latin American Spanish Right Total'
@@ -51,8 +30,49 @@ def main():
     channel_eleven_text = 'MOS'
     channel_twelve_text = 'MOS'
 
+    channels = [channel_one_text, channel_two_text, channel_three_text, channel_four_text, channel_five_text,
+                channel_six_text, channel_seven_text, channel_eight_text, channel_nine_text, channel_ten_text,
+                channel_eleven_text, channel_twelve_text]
+
+    channel_pairs = [
+        (channels[0], channels[6]),
+        (channels[1], channels[7]),
+        (channels[2], channels[8]),
+        (channels[3], channels[9]),
+        (channels[4], channels[10]),
+        (channels[5], channels[11]),
+    ]
+
+    canvas.saveState()
+    draw_section_one(canvas, channel_pairs)
+    canvas.restoreState()
+
+
+
+
+
+    canvas.saveState()
+    canvas.setFont("Helvetica-Oblique", 8)
+    canvas.translate(6.5 * inch, 8.9 * inch)
+    canvas.rotate(90)
+
+    canvas.drawString(0, 2.6 * inch, "Sony")
+    canvas.setFont("Helvetica", 10)
+    canvas.drawString(0, 2.4 * inch, "That's My Boy")
+
+    canvas.setFont("Helvetica", 5)
+    canvas.drawString(0, 2.25 * inch, "Version: Theatrical")
+    canvas.drawString(0, 2.16 * inch, "Latin American Spanish Foreign Language Master")
+    canvas.drawString(0, 2.07 * inch, "Aspect Ratio: 16x9 1.78 Full Frame")
+    canvas.drawString(0, 1.98 * inch, "Textless: Textless at Tail")
+
     current_height_one = 0
     current_height_two = 0
+
+
+    paragraph_style = ParagraphStyle('paraStyle')
+    paragraph_style.fontSize = 6
+    paragraph_style.leading = 8
 
     for iterator, channel_pair in enumerate([(channel_one_text, channel_seven_text), (channel_two_text, channel_eight_text),
                          (channel_three_text, channel_nine_text), (channel_four_text, channel_ten_text),
@@ -66,158 +86,155 @@ def main():
         current_height_one += height1
         current_height_two += height2
 
-        paragraph1.drawOn(c, 0, 1.7 * inch - current_height_one)
-        paragraph2.drawOn(c, 1.25 * inch, 1.7 * inch - current_height_two)
+        paragraph1.drawOn(canvas, 0, 1.7 * inch - current_height_one)
+        paragraph2.drawOn(canvas, 1.25 * inch, 1.7 * inch - current_height_two)
 
-    c.restoreState()
-
-    c.saveState()
-    c.setFont("Helvetica", 12)
-    c.translate(2.8 * inch, 8.75 * inch)
-    c.drawString(0, 2.6 * inch, '2G018653')
-
-    image = Image(os.path.dirname(os.path.realpath(__file__)) + '/2g_logo.png')
-    image.drawHeight = .70 * inch * image.drawHeight / image.drawWidth
-    image.drawWidth = .70 * inch
-
-    image.drawOn(c, 0, 1.8 * inch)
-
-    c.setFont('Helvetica', 6)
-    c.drawString(0, 1.6 * inch, '2g Digital Post, Inc.')
-    c.drawString(0, 1.5 * inch, '280 East Magnolia Blvd')
-    c.drawString(0, 1.4 * inch, 'Burbank, CA 91502')
-    c.drawString(0, 1.3 * inch, '818.863.8900')
-    c.drawString(0, 1.1 * inch, '2016-08-17')
-    c.drawString(0, 1.0 * inch, 'Std: 1080')
-    c.drawString(0, 0.9 * inch, 'FR: 23.98PsF')
-    c.drawString(0, 0.8 * inch, 'Part: 1 of 1')
-    c.drawString(0, 0.7 * inch, 'TRT: 114:17')
-
-    c.restoreState()
-
-
-
-
-
-    c.saveState()
-    c.setFont("Helvetica-Oblique", 8)
-    c.translate(6.5 * inch, 8.9 * inch)
-    # c.translate(4.1 * inch, 8.75 * inch)
-    c.rotate(90)
-    # c.translate(4.1 * inch, 8.75 * inch)
-    c.drawString(0, 2.6 * inch, "Sony")
-    c.setFont("Helvetica", 10)
-    c.drawString(0, 2.4 * inch, "That's My Boy")
-
-    c.setFont("Helvetica", 5)
-    c.drawString(0, 2.25 * inch, "Version: Theatrical")
-    c.drawString(0, 2.16 * inch, "Latin American Spanish Foreign Language Master")
-    c.drawString(0, 2.07 * inch, "Aspect Ratio: 16x9 1.78 Full Frame")
-    c.drawString(0, 1.98 * inch, "Textless: Textless at Tail")
-
-    current_height_one = 0
-    current_height_two = 0
-
-    for iterator, channel_pair in enumerate([(channel_one_text, channel_seven_text), (channel_two_text, channel_eight_text),
-                         (channel_three_text, channel_nine_text), (channel_four_text, channel_ten_text),
-                         (channel_five_text, channel_eleven_text), (channel_six_text, channel_twelve_text)]):
-        paragraph1 = Paragraph('CH{0:02d}: {1}'.format(iterator + 1, channel_pair[0]), paragraph_style)
-        width1, height1 = paragraph1.wrap(1.2 * inch, 50)
-
-        paragraph2 = Paragraph('CH{0:02d}: {1}'.format(iterator + 7, channel_pair[1]), paragraph_style)
-        width2, height2 = paragraph2.wrap(1.2 * inch, 50)
-
-        current_height_one += height1
-        current_height_two += height2
-
-        paragraph1.drawOn(c, 0, 1.7 * inch - current_height_one)
-        paragraph2.drawOn(c, 1.25 * inch, 1.7 * inch - current_height_two)
-
-    c.restoreState()
+    canvas.restoreState()
 
 
 
 
 
 
-    c.saveState()
+    canvas.saveState()
 
-    c.translate(.25 * inch, 7.375 * inch)
+    canvas.translate(.25 * inch, 7.375 * inch)
 
     image = Image(os.path.dirname(os.path.realpath(__file__)) + '/2g_logo.png')
     image.drawHeight = .5 * inch * image.drawHeight / image.drawWidth
     image.drawWidth = .5 * inch
 
-    image.drawOn(c, 0, inch * .43)
+    image.drawOn(canvas, 0, inch * .43)
 
-    c.setFont("Helvetica", 5)
-    c.drawString(.7 * inch, inch * .81, '2G Digital Post, Inc.')
-    c.drawString(.7 * inch, inch * .73, '280 East Magnolia Blvd.')
-    c.drawString(.7 * inch, inch * .65, 'Burbank, CA 91502')
-    c.drawString(.7 * inch, inch * .57, '818.863.8900')
+    canvas.setFont("Helvetica", 5)
+    canvas.drawString(.7 * inch, inch * .81, '2G Digital Post, Inc.')
+    canvas.drawString(.7 * inch, inch * .73, '280 East Magnolia Blvd.')
+    canvas.drawString(.7 * inch, inch * .65, 'Burbank, CA 91502')
+    canvas.drawString(.7 * inch, inch * .57, '818.863.8900')
 
-    c.setFont("Helvetica", 12)
-    c.drawString(1.6 * inch, inch * .7, '2G018653')
+    canvas.setFont("Helvetica", 12)
+    canvas.drawString(1.6 * inch, inch * .7, '2G018653')
 
-    c.drawString(2.5 * inch, inch * .7, "That's My Boy")
+    canvas.drawString(2.5 * inch, inch * .7, "That's My Boy")
 
-    c.setFont("Helvetica", 6)
+    canvas.setFont("Helvetica", 6)
 
-    c.drawString(4 * inch, inch * .81, "Part: 1 of 1")
-    c.drawString(4 * inch, inch * .73, "TRT: 114:17")
+    canvas.drawString(4 * inch, inch * .81, "Part: 1 of 1")
+    canvas.drawString(4 * inch, inch * .73, "TRT: 114:17")
 
-    c.restoreState()
+    canvas.restoreState()
 
     # 5P Labels
-    c.saveState()
+    canvas.saveState()
 
-    c.translate(.25 * inch, 5.90625 * inch)
+    canvas.translate(.25 * inch, 5.90625 * inch)
 
     image = Image(os.path.dirname(os.path.realpath(__file__)) + '/5P_vertical.png')
     image.drawHeight = .75 * inch
     image.drawWidth = .875 * inch
 
-    image.drawOn(c, 0, 0)
-    image.drawOn(c, .90625 * inch, 0)
+    image.drawOn(canvas, 0, 0)
+    image.drawOn(canvas, .90625 * inch, 0)
 
 
 
-    c.restoreState()
-    c.saveState()
+    canvas.restoreState()
+    canvas.saveState()
 
     translate_x = 2.5
     translate_y = 0.125
 
-    c.translate(translate_x * inch, translate_y * inch)
-    c.rotate(90)
+    canvas.translate(translate_x * inch, translate_y * inch)
+    canvas.rotate(90)
 
-    draw_side_binding(c)
-    draw_section_five(c)
+    draw_side_binding(canvas)
+    draw_section_five(canvas)
 
-    c.restoreState()
-    c.saveState()
+    canvas.restoreState()
+    canvas.saveState()
 
     translate_x = 5.2
 
-    c.translate(translate_x * inch, translate_y * inch)
-    c.rotate(90)
+    canvas.translate(translate_x * inch, translate_y * inch)
+    canvas.rotate(90)
 
-    draw_side_binding(c)
-    draw_section_six(c)
+    draw_side_binding(canvas)
+    draw_section_six(canvas)
 
-    c.restoreState()
-    c.saveState()
+    canvas.restoreState()
+    canvas.saveState()
 
     translate_x = 8.3
 
-    c.translate(translate_x * inch, translate_y * inch)
-    c.rotate(90)
+    canvas.translate(translate_x * inch, translate_y * inch)
+    canvas.rotate(90)
 
-    draw_side_binding(c)
-    draw_section_seven(c)
+    draw_side_binding(canvas)
+    draw_section_seven(canvas)
 
-    c.showPage()
-    c.save()
+    canvas.showPage()
+    canvas.save()
+
+
+def draw_section_one(canvas, channel_list):
+    canvas.setFont("Helvetica-Oblique", 8)
+    canvas.translate(.25 * inch, 8.75 * inch)
+    canvas.drawString(0, 2.6 * inch, "Sony")
+
+    canvas.setFont("Helvetica", 12)
+    canvas.drawString(0, 2.4 * inch, "That's My Boy")
+
+    canvas.setFont("Helvetica", 7)
+    canvas.drawString(0, 2.25 * inch, "Version: Theatrical")
+    canvas.drawString(0, 2.10 * inch, "Latin American Spanish Foreign Language Master")
+    canvas.drawString(0, 1.95 * inch, "Aspect Ratio: 16x9 1.78 Full Frame")
+    canvas.drawString(0, 1.8 * inch, "Textless: Textless at Tail")
+
+    canvas.setFont("Helvetica", 6)
+
+    paragraph_style = ParagraphStyle('paraStyle')
+    paragraph_style.fontSize = 6
+    paragraph_style.leading = 8
+
+    current_height_one = 0
+    current_height_two = 0
+
+    for iterator, channel_pair in enumerate(channel_list):
+        paragraph1 = Paragraph('CH{0:02d}: {1}'.format(iterator + 1, channel_pair[0]), paragraph_style)
+        width1, height1 = paragraph1.wrap(1.2 * inch, 50)
+
+        paragraph2 = Paragraph('CH{0:02d}: {1}'.format(iterator + 7, channel_pair[1]), paragraph_style)
+        width2, height2 = paragraph2.wrap(1.2 * inch, 50)
+
+        current_height_one += height1
+        current_height_two += height2
+
+        paragraph1.drawOn(canvas, 0, 1.7 * inch - current_height_one)
+        paragraph2.drawOn(canvas, 1.25 * inch, 1.7 * inch - current_height_two)
+
+    canvas.restoreState()
+
+    canvas.saveState()
+    canvas.setFont("Helvetica", 12)
+    canvas.translate(2.8 * inch, 8.75 * inch)
+    canvas.drawString(0, 2.6 * inch, '2G018653')
+
+    image = Image(os.path.dirname(os.path.realpath(__file__)) + '/2g_logo.png')
+    image.drawHeight = .70 * inch * image.drawHeight / image.drawWidth
+    image.drawWidth = .70 * inch
+
+    image.drawOn(canvas, 0, 1.8 * inch)
+
+    canvas.setFont('Helvetica', 6)
+    canvas.drawString(0, 1.6 * inch, '2g Digital Post, Inc.')
+    canvas.drawString(0, 1.5 * inch, '280 East Magnolia Blvd')
+    canvas.drawString(0, 1.4 * inch, 'Burbank, CA 91502')
+    canvas.drawString(0, 1.3 * inch, '818.863.8900')
+    canvas.drawString(0, 1.1 * inch, '2016-08-17')
+    canvas.drawString(0, 1.0 * inch, 'Std: 1080')
+    canvas.drawString(0, 0.9 * inch, 'FR: 23.98PsF')
+    canvas.drawString(0, 0.8 * inch, 'Part: 1 of 1')
+    canvas.drawString(0, 0.7 * inch, 'TRT: 114:17')
 
 
 def draw_side_binding(canvas):
