ea11698e4208f131df598ba5d3bc814445e3ab75
Tyler Standridge <tstand90@gmail.com>
2016-10-10 10:50:02 -0700

    Two more label sections

--- labels/hdcam_export_pdf.py
+++ labels/hdcam_export_pdf.py
@@ -101,7 +101,7 @@ c.restoreState()
 
 c.saveState()
 c.setFont("Helvetica-Oblique", 8)
-c.translate(6.5 * inch, 9.4 * inch)
+c.translate(6.5 * inch, 8.9 * inch)
 # c.translate(4.1 * inch, 8.75 * inch)
 c.rotate(90)
 # c.translate(4.1 * inch, 8.75 * inch)
@@ -114,6 +114,71 @@ c.drawString(0, 2.25 * inch, "Version: Theatrical")
 c.drawString(0, 2.16 * inch, "Latin American Spanish Foreign Language Master")
 c.drawString(0, 2.07 * inch, "Aspect Ratio: 16x9 1.78 Full Frame")
 c.drawString(0, 1.98 * inch, "Textless: Textless at Tail")
+
+current_height_one = 0
+current_height_two = 0
+
+for iterator, channel_pair in enumerate([(channel_one_text, channel_seven_text), (channel_two_text, channel_eight_text),
+                     (channel_three_text, channel_nine_text), (channel_four_text, channel_ten_text),
+                     (channel_five_text, channel_eleven_text), (channel_six_text, channel_twelve_text)]):
+    paragraph1 = Paragraph('CH{0:02d}: {1}'.format(iterator + 1, channel_pair[0]), paragraph_style)
+    width1, height1 = paragraph1.wrap(1.2 * inch, 50)
+
+    paragraph2 = Paragraph('CH{0:02d}: {1}'.format(iterator + 7, channel_pair[1]), paragraph_style)
+    width2, height2 = paragraph2.wrap(1.2 * inch, 50)
+
+    current_height_one += height1
+    current_height_two += height2
+
+    paragraph1.drawOn(c, 0, 1.7 * inch - current_height_one)
+    paragraph2.drawOn(c, 1.25 * inch, 1.7 * inch - current_height_two)
+
+c.restoreState()
+
+
+
+
+
+
+c.saveState()
+
+c.translate(.25 * inch, 7.375 * inch)
+
+image = Image(os.path.dirname(os.path.realpath(__file__)) + '/2g_logo.png')
+image.drawHeight = .5 * inch * image.drawHeight / image.drawWidth
+image.drawWidth = .5 * inch
+
+image.drawOn(c, 0, inch * .43)
+
+c.setFont("Helvetica", 5)
+c.drawString(.7 * inch, inch * .81, '2G Digital Post, Inc.')
+c.drawString(.7 * inch, inch * .73, '280 East Magnolia Blvd.')
+c.drawString(.7 * inch, inch * .65, 'Burbank, CA 91502')
+c.drawString(.7 * inch, inch * .57, '818.863.8900')
+
+c.setFont("Helvetica", 12)
+c.drawString(1.6 * inch, inch * .7, '2G018653')
+
+c.drawString(2.5 * inch, inch * .7, "That's My Boy")
+
+c.setFont("Helvetica", 6)
+
+c.drawString(4 * inch, inch * .81, "Part: 1 of 1")
+c.drawString(4 * inch, inch * .73, "TRT: 114:17")
+
+c.restoreState()
+
+# 5P Labels
+c.saveState()
+
+c.translate(.25 * inch, 5.90625 * inch)
+
+image = Image(os.path.dirname(os.path.realpath(__file__)) + '/5P_vertical.png')
+image.drawHeight = .75 * inch
+image.drawWidth = .875 * inch
+
+image.drawOn(c, 0, 0)
+image.drawOn(c, .90625 * inch, 0)
 
 c.showPage()
 c.save()
