074d38579a35c7ae466981793806775fc38907ba
Tyler Standridge <tstand90@gmail.com>
2016-11-21 08:51:28 -0800

    Removing DeliveryTaskInspectWdg for now

--- widgets/generic_task_inspect_wdg.py
+++ widgets/generic_task_inspect_wdg.py
@@ -14,9 +14,11 @@ class GenericTaskInspectWdg(BaseRefreshWdg):
         div_wdg = DivWdg()
         div_wdg.set_id('outer_task_inspect_{0}'.format(self.task_sobject.get_code()))
 
-        if self.task_sobject.get('process').lower() == 'edel: deliver':
-            div_wdg.add(DeliveryTaskInspectWdg(search_key=self.task_sobject.get_search_key()))
-        else:
-            div_wdg.add(TaskInspectWdg(search_key=self.task_sobject.get_search_key()))
+        # if self.task_sobject.get('process').lower() == 'edel: deliver':
+            # div_wdg.add(DeliveryTaskInspectWdg(search_key=self.task_sobject.get_search_key()))
+        # else:
+            # div_wdg.add(TaskInspectWdg(search_key=self.task_sobject.get_search_key()))
+
+        div_wdg.add(TaskInspectWdg(search_key=self.task_sobject.get_search_key()))
 
         return div_wdg
