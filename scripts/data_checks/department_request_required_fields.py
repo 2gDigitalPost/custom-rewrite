fd99a7147ff58eaa5cba81abcabe0211967538bc
Tyler Standridge <tstand90@gmail.com>
2016-12-16 12:13:05 -0800

    Inserting a default status if none is provided

--- scripts/data_checks/department_request_required_fields.py
+++ scripts/data_checks/department_request_required_fields.py
@@ -13,8 +13,26 @@ def insert_data_check(server=None, input_data=None):
     if not department_request_sobject.get('description'):
         raise TacticException("Description field is required.")
 
+    # If 'status' is not set, set it to 'in_progress' by default
+    if not department_request_sobject.get('status'):
+        status = 'in_progress'
+    else:
+        status = None
+
     # If 'login' is not in the inserted sobject, insert it using the logged in user's name
     if not department_request_sobject.get('login'):
         login = Environment.get_security().get_login().get_login()
+    else:
+        login = None
 
-        server.update(department_request_sobject.get('__search_key__'), {'login': login})
+    # If either status or login was set, and update is needed
+    if status or login:
+        update_dictionary = {}
+
+        if status:
+            update_dictionary['status'] = status
+        if login:
+            update_dictionary['login'] = login
+
+        # Send the update data
+        server.update(department_request_sobject.get('__search_key__'), update_dictionary)
