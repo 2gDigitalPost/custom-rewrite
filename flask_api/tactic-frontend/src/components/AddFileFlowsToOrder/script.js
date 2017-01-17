c22bf3b7e9ff379bdd3487cfc92f8aa5424c07ed
Tyler Standridge <tstand90@gmail.com>
2017-01-17 08:59:09 -0800

    Can submit changes in file flows

--- flask_api/tactic-frontend/src/components/AddFileFlowsToOrder/script.js
+++ flask_api/tactic-frontend/src/components/AddFileFlowsToOrder/script.js
@@ -72,7 +72,7 @@ export default {
         self.showModal.push(false)
       }
     },
-    fileFlowSelected(fileFlow) {
+    fileFlowSelected: function (fileFlow) {
       // Start by setting all packages to not externally selected
       this.changeAllPackagesExternallySelected(false)
 
@@ -86,7 +86,7 @@ export default {
         }
       }
     },
-    togglePackageExternallySelected(packageCode) {
+    togglePackageExternallySelected: function (packageCode) {
       for (let i = 0; i < this.selectable_packages.length; i++) {
         let currentPackage = this.selectable_packages[i]
 
@@ -95,17 +95,101 @@ export default {
         }
       }
     },
-    changeAllPackagesExternallySelected(newStatus) {
+    changeAllPackagesExternallySelected: function (newStatus) {
       for (let i = 0; i < this.selectable_packages.length; i++) {
         this.selectable_packages[i].externallySelected = newStatus
       }
     },
-    packageSelected(selectedPackage) {
+    packageSelected: function (selectedPackage) {
       selectedPackage.selected = !selectedPackage.selected
+    },
+    findFileFlowToPackage: function (fileFlowCode, packageCode) {
+      let self = this
+
+      let fileFlowToPackagesForFileFlow = self.fileFlowToPackages[fileFlowCode]
+
+      for (let i = 0; i < fileFlowToPackagesForFileFlow.length; i++) {
+        let fileFlowToPackage = fileFlowToPackagesForFileFlow[i]
+
+        if (fileFlowToPackage.package_code == packageCode) {
+          return fileFlowToPackage.code
+        }
+      }
+
+      return null
+    },
+    submitFileFlowToPackage: function (fileFlowCode, packageCode) {
+      let self = this
+
+      axios.post('/api/v1/file-flow-to-package',
+        JSON.stringify({
+          'file_flow_to_package': {
+            'file_flow_code': fileFlowCode,
+            'package_code': packageCode
+          },
+          'token': localStorage.tactic_token
+        }), {
+          headers: {
+            'Content-Type': 'application/json;charset=UTF-8'
+          }
+        }
+      )
+      .then(function (response) {
+        if (response.data) {
+          if (response.data.status === 200) {
+            self.$router.go(self.$router.currentRoute)
+          }
+        }
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+    },
+    deleteFileFlowToPackage: function (codeToDelete) {
+      let self = this
+
+      axios.delete('/api/v1/file-flow-to-package/' + codeToDelete,
+        {
+          headers: {
+            'Content-Type': 'application/json;charset=UTF-8'
+          },
+          params: {
+            token: localStorage.tactic_token,
+          }
+        }
+      )
+      .then(function (response) {
+        if (response.data) {
+          if (response.data.status === 200) {
+            self.$router.go(self.$router.currentRoute)
+          }
+        }
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+    },
+    submitNewLinks: function () {
+      var self = this
+
+      if (self.selectedFileFlow !== null) {
+        for (let i = 0; i < self.selectable_packages.length; i++) {
+          let selectedPackage = self.selectable_packages[i]
+          let selectedPackageCode = selectedPackage.package.code
+
+          if (selectedPackage.selected && selectedPackage.externallySelected) {
+            let codeToDelete = self.findFileFlowToPackage(self.selectedFileFlow, selectedPackageCode)
+
+            self.deleteFileFlowToPackage(codeToDelete)
+          }
+          else if (selectedPackage.selected) {
+            self.submitFileFlowToPackage(self.selectedFileFlow, selectedPackageCode)
+          }
+        }
+      }
     }
   },
   beforeMount: function () {
     this.loadOrder()
-    // this.setupShowModalArray()
   }
 }
