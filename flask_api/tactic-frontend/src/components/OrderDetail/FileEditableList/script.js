20765305aa9b4152904f0c66f84584b6b297b75d
Tyler Standridge <tstand90@gmail.com>
2017-02-20 13:16:12 -0800

    Removed unused code for adding files

--- flask_api/tactic-frontend/src/components/OrderDetail/FileEditableList/script.js
+++ flask_api/tactic-frontend/src/components/OrderDetail/FileEditableList/script.js
@@ -20,61 +20,12 @@ export default {
   data () {
     return {
       creatingNewFile: false,
-      newFileName: null,
-      newFilePath: null,
-      newFileClassification: null,
-      classificationOptions: ['Source', 'Intermediate', 'Deliverable'],
-      selectedFiles: [],
-      errors: []
+      importingFiles: false
     }
   },
   methods: {
     filterFilesByClassification: function (classification) {
       return _.filter(this.files, function(file) { return _.lowerCase(file['classification']) === classification })
-    },
-    submitNewFile: function () {
-      let self = this
-
-      self.errors = []
-
-      if (!self.newFileName) {
-        self.errors.push({type: 'name', message: 'The field "Name" is required.'})
-      }
-      if (!self.newFilePath) {
-        self.errors.push({type: 'filePath', message: 'The field "Path" is required.'})
-      }
-      if (!self.newFileClassification) {
-        self.errors.push({type: 'classification', message: 'The field "Classification" is required.'})
-      }
-
-      if (self.errors.length > 0) return
-
-      let apiURL = '/api/v1/file'
-      let originFileCodes = _.map(self.selectedFiles, 'code')
-      let jsonToSend = {
-        'token': localStorage.tactic_token,
-        'file': {
-          'name': self.newFileName,
-          'file_path': self.newFilePath,
-          'classification': self.newFileClassification
-        },
-        'order_code': self.orderCode,
-        'origin_file_codes': originFileCodes
-      }
-
-      axios.post(apiURL, JSON.stringify(jsonToSend), {
-        headers: {
-          'Content-Type': 'application/json;charset=UTF-8'
-        },
-      })
-      .then(function (response) {
-        if (response.status === 200) {
-          bus.$emit('new-file-added-to-order')
-        }
-      })
-      .catch(function (error) {
-        console.log(error)
-      })
     },
     cancelEdit: function () {
       this.creatingNewFile = false
@@ -89,15 +40,6 @@ export default {
     },
     deliverableFiles: function () {
       return this.filterFilesByClassification('deliverable')
-    },
-    nameError: function () {
-      return _.includes(_.map(this.errors, 'type'), 'name')
-    },
-    filePathError: function () {
-      return _.includes(_.map(this.errors, 'type'), 'filePath')
-    },
-    classificationError: function () {
-      return _.includes(_.map(this.errors, 'type'), 'classification')
     }
   },
   created() {
